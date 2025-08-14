014537b1d7f0abc43d20067025102ca36d5d3eff
Wealthometer <wealthhajoh87@gmail.com>
2025-08-14 12:31:33 +0100

    Update

---
+++ ai_trip_planner/app/_components/Header.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Image from 'next/image'
+// import path from 'path'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+
+const menuOptions = [
+    {
+        name : 'Home',
+        path : '/'
+    },
+    {
+        name : 'Pricing',
+        path : '/pricing'
+    },
+    {
+        name : 'Contact',
+        path : '/contact-us'
+    }
+]
+
+const Header = () => {
+  return (
+    <div className='flex justify-between items-center p-5'>
+        <div className='flex gap-2 items-center'>
+            <Image src={'/logo.svg'} alt='logo' width={30} height={30} />
+            <h2 className='font-bold text-2xl'>AI TRIP PLANNER</h2>
+        </div>
+
+        <div className='flex gap-5 items-center'>
+            {menuOptions.map((menu, index) => (
+                <Link href={menu.path}>
+                    <h2 className='text-lg hover:scale-105 transition-all'>
+                        {menu.name}
+                    </h2>
+                </Link>
+            ))}
+        </div>
+
+        <Button>
+            Get Started
+        </Button>
+    </div>
+  )
+}
+
+export default Header
--- ai_trip_planner/app/page.tsx
+++ ai_trip_planner/app/page.tsx
@@ -1,7 +1,11 @@
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import '../app/globals.css'
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
-
+      
     </div>
   );
 }
